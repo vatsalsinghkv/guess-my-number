edb35aa34484d50154ce68893ae7d0a9634746bd
vatsal <vatsalsinghkv@gmail.com>
2023-12-26 10:36:23 +0530

    feat: game over screen finish

--- src/screens/GameEnd.tsx
+++ src/screens/GameEnd.tsx
@@ -12,22 +12,30 @@ type Props = {
 export default function GameEnd({ onScreenChange, num }: Props) {
   return (
     <View style={styles.container}>
-      <Title>Game Over!</Title>
+      <View style={styles.resultContainer}>
+        <Title>Game Over!</Title>
 
-      <View style={styles.center}>
-        <View style={styles.imgContainer}>
-          <Image
-            style={styles.imgContainer}
-            source={require('../assets/success.png')}
-          />
+        <View style={styles.center}>
+          <View style={styles.imgContainer}>
+            <Image
+              style={styles.imgContainer}
+              source={require('../assets/success.png')}
+            />
+          </View>
         </View>
+
+        {/* Applying text styles on the parent text container also affects the child text containers. */}
+        {/* But this doesn't happen when you apply text styles to view it doesn't affect the child text containers. */}
+        <Text style={styles.textSummary}>
+          Your phone needed <Text style={styles.textHighlight}>{num}</Text>{' '}
+          rounds to guess the number{' '}
+          <Text style={styles.textHighlight}>{num}</Text>
+        </Text>
       </View>
 
-      <Button onPress={() => onScreenChange('start')}>Play Again</Button>
-
-      <Text>
-        Your phone needed 5 rounds to guess the number <Text>{num}</Text>
-      </Text>
+      <Button variant='primary' onPress={() => onScreenChange('start')}>
+        New Game
+      </Button>
     </View>
   );
 }
@@ -35,7 +43,10 @@ export default function GameEnd({ onScreenChange, num }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    gap: 30,
+    justifyContent: 'space-between',
+  },
+  resultContainer: {
+    gap: 36,
   },
   center: { alignItems: 'center' },
   imgContainer: {
@@ -50,5 +61,16 @@ const styles = StyleSheet.create({
   img: {
     height: '100%',
     width: '100%',
+  },
+  // Text Styles
+  textSummary: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontFamily: 'open-sans',
+  },
+  textHighlight: {
+    color: Colors.primary[500],
+    fontWeight: 'bold',
+    fontFamily: 'open-sans-bold',
   },
 });
